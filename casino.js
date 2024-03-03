3f85d569c891a99c8b94113a5cc611b8fff85b7f
tunnelpr0 <154389351+donatnagye@users.noreply.github.com>
2024-03-04 05:04:05 +0700

    Update casino.js

--- casino.js
+++ casino.js
@@ -42,7 +42,9 @@ const yes = () => {
         composition.hidden = false
         _window.style.backgroundImage = `url('${image.src}')`
         document.body.style.backgroundSize = 'auto'
-        document.body.style.backgroundRepeat = 'no-repeat';
+        document.body.style.backgroundRepeat = 'no-repeat'
+        document.body.style.marginLeft = 'auto'
+        document.body.style.marginRight = 'auto'
         document.body.style.backgroundImage = `url('${image.src}')`
         document.getElementById('website').textContent = 'Bondage gay website'
         const currency = document.getElementsByClassName('currency')
